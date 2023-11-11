f8f13a22f6261b0ee6f023ad385332c6331001af
Razdvizh <shustov-jenja@rambler.ru>
2023-11-10 21:51:30 -0600

    Revert "Revert "Add growing pumpkin""
    This reverts commit 53a33a2be74cdd55d3677bbca66d66317ff485dc.

--- Source/ProjectPumpkin/Public/MortalCharacter.h
+++ Source/ProjectPumpkin/Public/MortalCharacter.h
@@ -19,6 +19,12 @@ public:
 	// Sets default values for this character's properties
 	AMortalCharacter(const FObjectInitializer& ObjectInitialier);
 
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, Category = "Mortal Character")
+	FORCEINLINE UHealthComponent* GetHealthComponent() const { return Health; }
+
+	UFUNCTION(BlueprintCallable, Category = "Mortal Character")
+	FORCEINLINE FDamageInfo GetDamageInfo() const { return DamageInfo; }
+
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 
 protected:
